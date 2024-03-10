8a321d37fedf3c223fa59f6703ef89aa9c2479ac
Lorenzo Manfredi <manfredilorenzo@itis-molinari.eu>
2024-03-10 15:49:53 +0100

    gestione accedi

--- public/accedi.php
+++ public/accedi.php
@@ -1,15 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <body>
+    <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+require_once('connessione.php');
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST['nome'];
-
     $sql = "SELECT * FROM giocatore WHERE nome = '$nome'";
+    $sqlInsert = "INSERT INTO giocatore (nome) VALUES ('$nome')";
 
-    if (mysqli_query($connessione, $sql)) {
-        // Utente trovato, esegui il reindirizzamento
-        header('Location: /public/preGame.html');
-        exit();  // Assicurati di terminare lo script qui
+    $result = mysqli_query($connessione, $sql);
+
+    if ($result) {
+        $numeroRighe = mysqli_num_rows($result);
+
+        if ($numeroRighe > 0) {
+            // Utente trovato, esegui il reindirizzamento
+            header('Location: ./preGame.html');
+            exit();  // Assicurati di terminare lo script qui
+        } else {
+            // Utente non trovato, esegui l'inserimento
+            if (mysqli_query($connessione, $sqlInsert)) {
+                echo "Inserito";
+            } else {
+                echo "Errore nell'inserimento: " . mysqli_error($connessione);
+            }
+        }
     } else {
         echo "Errore nell'esecuzione della query: " . mysqli_error($connessione);
     }
 }
 ?>
+
+    </body>
+</html>
