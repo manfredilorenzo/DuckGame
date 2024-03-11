6a0f6fe5cb2b6ec2f20a9a7c321ac4e294185d98
Lorenzo Manfredi <manfredilorenzo@itis-molinari.eu>
2024-03-11 09:34:02 +0100

    non funziona la post voglio morire

--- public/accedi.php
+++ public/accedi.php
@@ -1,24 +1,21 @@
-<!DOCTYPE html>
-<html>
-    <body>
-    <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+<?php
 require_once('connessione.php');
-
+echo "Ciao";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nome = $_POST['nome'];
+
+    $nome = $_POST['Nome'];
     $sql = "SELECT * FROM giocatore WHERE nome = '$nome'";
     $sqlInsert = "INSERT INTO giocatore (nome) VALUES ('$nome')";
-
+echo "ciao";
     $result = mysqli_query($connessione, $sql);
+    echo $result;
 
     if ($result) {
         $numeroRighe = mysqli_num_rows($result);
-
-        if ($numeroRighe > 0) {
+        echo $numeroRighe;
+        /*if ($numeroRighe > 0) {
             // Utente trovato, esegui il reindirizzamento
-            header('Location: ./preGame.html');
+            header('location: /preGame.html');
             exit();  // Assicurati di terminare lo script qui
         } else {
             // Utente non trovato, esegui l'inserimento
@@ -27,12 +24,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             } else {
                 echo "Errore nell'inserimento: " . mysqli_error($connessione);
             }
-        }
+        }*/
     } else {
         echo "Errore nell'esecuzione della query: " . mysqli_error($connessione);
     }
 }
 ?>
-
-    </body>
-</html>
