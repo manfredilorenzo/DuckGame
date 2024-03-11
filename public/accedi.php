5ffc0f47ecd11b2861edf8ddfadc8cdf81b07db6
Lorenzo Manfredi <manfredilorenzo@itis-molinari.eu>
2024-03-11 09:53:28 +0100

    ACCEDI FUNZIONA AVE CESARE

--- public/accedi.php
+++ public/accedi.php
@@ -2,18 +2,19 @@
 require_once('connessione.php');
 echo "Ciao";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo "ciao";
     $nome = $_POST['Nome'];
     $sql = "SELECT * FROM giocatore WHERE nome = '$nome'";
     $sqlInsert = "INSERT INTO giocatore (nome) VALUES ('$nome')";
     $result = mysqli_query($connessione, $sql);
-    echo $result;
+    var_dump($result);
 
     if ($result) {
         $numeroRighe = mysqli_num_rows($result);
         echo $numeroRighe;
         if ($numeroRighe > 0) {
             // Utente trovato, esegui il reindirizzamento
-            header('location: /preGame.html');
+            header('location: ./preGame.html');
             exit();  // Assicurati di terminare lo script qui
         } else {
             // Utente non trovato, esegui l'inserimento
